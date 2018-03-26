01d45281a7c94586ae2d89985f8e858a08741e03
Desty NA <destynursyifaa@gmail.com>
2018-03-26 04:27:05 -0700

    mengisi nilai atribut

--- src/praktek1/SapiAksi.java
+++ src/praktek1/SapiAksi.java
@@ -14,6 +14,24 @@ public class SapiAksi {
         Sapi Ku = new Sapi();
         Sapi Pak_Lurah = new Sapi();
         Sapi Pak_RT = new Sapi();
+        
+        Ku.Jenis_Sapi="Sapi Perah";
+        Ku.Warna_Sapi="Coklat";
+        Ku.Bentuk_Sapi="Besar";
+        Ku.Gigi_Sapi="Gigi Seri";
+        Ku.Ekor_Sapi="Ekor Panjang";
+        
+        Pak_Lurah.Jenis_Sapi="Sapi Ongole";
+        Pak_Lurah.Warna_Sapi="Hitam";
+        Pak_Lurah.Bentuk_Sapi="Kecil";
+        Pak_Lurah.Gigi_Sapi="Gigi Geraham Belakang";
+        Pak_Lurah.Ekor_Sapi="Ekor Pendek";
+        
+        Pak_RT.Jenis_Sapi="Sapi Potong";
+        Pak_RT.Warna_Sapi="Putih Bercak Hitam";
+        Pak_RT.Bentuk_Sapi="Sedang";
+        Pak_RT.Gigi_Sapi="Gigi Rahang";
+        Pak_RT.Ekor_Sapi="Ekor Berbulu";
     }
     
 }
