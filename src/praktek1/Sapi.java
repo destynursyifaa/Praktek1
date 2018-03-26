483f72c622c3b1a3da4e0b3c4f38111e0178a9ac
Desty NA <destynursyifaa@gmail.com>
2018-03-26 04:06:25 -0700

    membuat atribut dan behavior

--- src/praktek1/Sapi.java
+++ src/praktek1/Sapi.java
@@ -10,5 +10,19 @@ package praktek1;
  * @author DestyNP
  */
 public class Sapi {
+    String Jenis_Sapi;
+    String Warna_Sapi;
+    String Bentuk_Sapi;
+    String Gigi_Sapi;
+    String Ekor_Sapi;
+    
+    void cetakInfo(){
+        System.out.println("Jenis_Sapi :"+Jenis_Sapi);
+        System.out.println("Warna_Sapi :"+Warna_Sapi);
+        System.out.println("Bentuk_Sapi :"+Bentuk_Sapi);
+        System.out.println("Gigi_Sapi :"+Gigi_Sapi);
+        System.out.println("Ekor_Sapi :"+Ekor_Sapi);
+        
+    }
     
 }
